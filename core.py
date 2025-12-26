be281b94d93ca367f85b2001916a1f4071d12e8f
mayssaGH <mayssagnm@gmail.com>
2025-12-26 18:51:26 +0100

    refactor: rename function names to align with Python conventions

--- core.py
+++ core.py
@@ -1,7 +1,7 @@
 import hmac
 import time
 
-def DT(hs): # Dynamic Truncation
+def dt(hs): # Dynamic Truncation
     offset = hs[-1] & 0b1111 # or & 15 or & 0xf to get last 4 bits
     p = hs[offset:offset+4]
     # Get last 31 bits from 32 bits (4 bytes)
@@ -11,7 +11,7 @@ def DT(hs): # Dynamic Truncation
         s_bits.append(b & 0xff) # return 8 bits exactly (apparently a byte can have more than 8 bits)
     return s_bits
 
-def HOTP(k: bytes, c, digits=6, algorithm="sha1"):
+def hotp(k: bytes, c, digits=6, algorithm="sha1"):
     """HTOP :
     k is the shared key,
     C is the counter value,
@@ -21,12 +21,12 @@ def HOTP(k: bytes, c, digits=6, algorithm="sha1"):
     counter_bytes = c.to_bytes(8, 'big') # 8-byte big-endian
     hs_hmac = hmac.new(k, counter_bytes, algorithm)
     hs = hs_hmac.digest()
-    s_bits = DT(hs)
+    s_bits = dt(hs)
     s_num = int.from_bytes(s_bits, 'big')
     otp = s_num % (10 ** digits)
     return str(otp).zfill(digits)
 
-def TOTP(k: bytes, digits=6, timestep=30, t0=0, algorithm="sha1", unix_time=None):
+def totp(k: bytes, digits=6, timestep=30, t0=0, algorithm="sha1", unix_time=None):
     """TOTP with specific Unix timestamp :
     k is the shared key,
     digits control the response length,
@@ -39,4 +39,4 @@ def TOTP(k: bytes, digits=6, timestep=30, t0=0, algorithm="sha1", unix_time=None
     if unix_time is None:
         unix_time = int(time.time())
     time_steps = (unix_time - t0) // timestep
-    return HOTP(k, time_steps, digits, algorithm)
+    return hotp(k, time_steps, digits, algorithm)
